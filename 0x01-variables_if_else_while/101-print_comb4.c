9c049d1ee0ad4e40d807cf7b903052cb43d89d69
Steph-en <appahstephen9@gmail.com>
2023-07-16 22:45:32 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b, c, d;
+
+	for (b = '0'; b < '9'; b++)
+	{
+		for (c = b + 1; c <= '9'; c++)
+		{
+			for (d = c + 1; d <= '9'; d++)
+			{
+				if ((c != b) !=d)
+				{
+					putchar(b);
+					putchar(c);
+					putchar(d);
+					if (b == '7' && c == '8')
+					continue;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
